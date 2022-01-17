52acadc4c9d8e93d272f960e54980278007a5ad2
Michelle Xie <michellexxie@ucla.edu>
2022-01-17 15:59:45 -0800

    Add files via upload

---
+++ Project1 - Band Name Generator.py
@@ -0,0 +1,15 @@
+#=======Band Name Generator - Michelle Yan Xie=======
+
+#1. Create a greeting for your program.
+print("Welcome to the bank name generator.")
+
+#2. Ask the user for the city that they grew up in.
+city = input("Which city did you grow up in?")
+print(city)
+
+#3. Ask the user for the name of a pet.
+pet = input("What is the name of a pet")
+print(pet)
+
+#4. Combine the name of their city and pet and show them their band name.
+print("Your band name could be "+city+" "+pet)
