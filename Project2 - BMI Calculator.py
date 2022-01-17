2c89650c06ecf86d8e9887d3b5d980f54dd34e53
Michelle Xie <michellexxie@ucla.edu>
2022-01-17 15:59:59 -0800

    Add files via upload

---
+++ Project2 - BMI Calculator.py
@@ -0,0 +1,7 @@
+#=======BMI Calculator - Michelle Yan Xie=======
+
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+
+bmi=int(weight)/(float(height)**2)
+print(bmi)
