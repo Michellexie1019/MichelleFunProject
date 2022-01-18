8a5c3f18f6c8fda301b53e8692847eb647c65f0e
Michelle Xie <michellexxie@ucla.edu>
2022-01-17 16:00:11 -0800

    Add files via upload

---
+++ Project3 - Urban Life in Weeks.py
@@ -0,0 +1,8 @@
+#=======Urban Life in Weeks - Michelle Yan Xie=======
+
+age = input("What is your current age?")
+
+days=365*90-int(age)*365
+weeks=52*90-int(age)*52
+months=90*12-int(age)*12
+print(f"You have {days} days, {weeks} weeks, and {months} months left.")
